c2771c1f48d91c5dcf7b3473b298616a1a369abb
cryptic <crypticmetadev@gmail.com>
2022-11-18 17:51:40 +0530

    updates

--- src/Ashu.js
+++ src/Ashu.js
@@ -21,23 +21,27 @@ const Ashu = () => {
     //  },[input1,input2])
 
   return (
-    <div className="flex flex-col mt-8">
-      <div className="bg-green-00 text-center text-3xl font-bold"> </div>
-      <div className="bg-red-00 text-center mt-2 space-x-2">
+    <div className="flex flex-col justify-center bg-gray-500 h-screen w-full">
+      <div className="text-center">
+        <div className='space-x-4'>
         {/* <button className='bg-yellow-500 p-1 rounded' onClick={() => setchange(change+2)}  >Click High</button>
         <button className='bg-yellow-500 p-1 rounded' onClick={ () => setchange(change-2)} >Click Low </button> */}
         <input type='number' value={input1}
          onChange={(e) => {setinput1(e.target.value)}}
-        className='border-2 border-black rounded'/>
+        className='border border-black rounded p-2 shadow-md shadow-gray-300'/>
         <input type='number' value={input2}
          onChange={(e) => {setinput2(e.target.value)}}
-        className='border-2 border-black rounded'/>
-        <button className='bg-yellow-500 p-1 rounded'onClick={result} > Click here</button>
-
-        <div className='text-2xl font-bold'>ADD:{add}</div>
-        <div>Multiply:{' '}{multi}</div>
-        <div>Sub:{' '}{sub}</div>
-        <div>Division:{' '}{division}, Remainder:{remainder}</div>
+        className='border border-black rounded p-2 shadow-md shadow-gray-300'/>
+        <button className=' border border-black bg-yellow-500 font-semibold hover:text-white shadow-inner shadow-white p-1 rounded-lg'onClick={result} > Click here</button>
+        </div>
+        <div className='bg-red-00 w-full justify-center flex lg:p-8'>
+         <div className='bg-gray-700 lg:w-6/12 lg:p-6 rounded-lg shadow-inner shadow-white'>
+        <div className='text-xl font-bold text-white'>ADD: {add}</div>
+        <div className='text-white text-xl font-bold'>Multiply:{' '}{multi}</div>
+        <div className='text-white text-xl font-bold'>Sub:{' '}{sub}</div>
+        <div className='text-white text-xl font-bold'>Division: {division}, Remainder: {remainder}</div>
+        </div>
+        </div>
       </div>
     </div>
   );
